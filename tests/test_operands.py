81ecd15606ff07b20ed64cb6ca382f9c3eae65d9
AzizNadirov <eziznadirov@gmail.com>
2024-08-17 12:58:07 +0400

    feat: added operands_factory

--- tests/test_operands.py
+++ tests/test_operands.py
@@ -1,12 +1,18 @@
 import unittest
 from unittest import TestCase
 
-from src.pytemplate.domain.models import Operands
+from src.pytemplate.domain.models import Operands, operands_factory
 
 
 class TestOperands(TestCase):
     def test_types(self):
         self.assertIsInstance(Operands(first_operand=1, second_operand=1), Operands)
+
+    def test_operands_factory(self):
+        operands = operands_factory(first_operand=1, second_operand=1)
+        self.assertIsInstance(operands, Operands)
+        self.assertEqual(operands.first_operand, 1)
+        self.assertEqual(operands.second_operand, 1)
 
 
 if __name__ == "__main__":
