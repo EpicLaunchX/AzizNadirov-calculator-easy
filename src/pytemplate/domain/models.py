eac6c88f8b0aca06a95045904bf4f4f29a360d7b
AzizNadirov <eziznadirov@gmail.com>
2024-08-17 12:21:39 +0400

    fix: changed to dataclass

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -1,7 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Operands:
     """sci operands"""
-
-    def __init__(self, first_operand: int, second_operand: int) -> None:
-        """cool init"""
-        self.first_operand = first_operand
-        self.second_operand = second_operand
+    first_operand: int
+    second_operand: int
